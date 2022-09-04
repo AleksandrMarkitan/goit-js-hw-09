1d93716226b568e4e7ec4d41060dce5eb46bdbb4
AleksandrMarkitan <0688138@gmail.com>
2022-09-04 22:32:11 +0300

    Виконав завдання 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  body: document.querySelector('body'),
+  startButton: document.querySelector('button[data-start]'),
+  stopButton: document.querySelector('button[data-stop]'),
+};
 
+refs.startButton.addEventListener('click', onStartButtonHandler);
+refs.stopButton.addEventListener('click', onStopButtonHandler);
+
+let timerId = null;
+function onStartButtonHandler() {
+  refs.startButton.setAttribute('disabled', true);
+  timerId = setInterval(
+    () => (refs.body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+}
+
+function onStopButtonHandler() {
+  clearInterval(timerId);
+  refs.startButton.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
